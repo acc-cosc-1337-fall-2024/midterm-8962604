4a30b5adc06036f66b500348c346f515fd81cf51
8962604 <omar.farooq@g.austincc.edu>
2024-10-14 15:33:03 +0000

    fib series

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -3,4 +3,20 @@
 bool test_config()
 {
     return true;
+
+}
+
+int get_fib_number(int n)
+{
+    if (n == 0)
+    return 0;
+    if (n == 1)
+    return 1;
+    int a = 0, b = 1, fib = 0;
+    for (int i = 2; i <= n; ++i) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    return fib;
 }
--- src/question_1/question1.h
+++ src/question_1/question1.h
@@ -1,1 +1,5 @@
 bool test_config();
+#ifndef FIBONACCI_H
+#define FIBONACCI_Hint 
+int get_fib_number(int n); 
+#endif 
